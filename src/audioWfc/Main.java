cb94637ee232885ff812da4ec0ff3a8fdf4e09c6
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2023-01-22 10:15:50 +0100

    Deleted outdated demos

--- src/audioWfc/Main.java
+++ src/audioWfc/Main.java
@@ -26,15 +26,7 @@ import static audioWfc.musicTheory.Note.*;
 
 public class Main {
     public static void main(String[] args) {
-        //basicWfcDemo();
         chordsAndNotesDemo();
-    }
-
-    private static void basicWfcDemo() {
-        List<Integer> example = List.of(0,0,1,2,3,3);
-        WFC<Integer> wfc = new WFC<>(example);
-
-        System.out.println(wfc.generate(List.of(Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(1), Optional.empty(), Optional.empty())));
     }
 
     private static void chordsAndNotesDemo() {
@@ -60,31 +52,5 @@ public class Main {
             List<Note> melodySegment = noteWFC.generate();
             System.out.println(chord + " - " + melodySegment);
         }
-
-
-
-                /*
-        Set<NeighborPair<Chord>> chordPairs = ConstraintUtils
-                .applyHardConstraint(WFCUtils.allCombinationsNoRepeats(chordOptions),
-                        new ChordStepSizeHardConstraint(Set.of(3,4,5)));
-        //System.out.println("Chord pairs: " + chordPairs);
-
-        WFC<Chord> chordWFC = new WFC<>(chordPairs);
-        List<Chord> chordProgression = chordWFC.generate(8);
-        System.out.println("Chord progression: " + chordProgression);
-
-        Set<Note> noteOptions = new NotesInKeyConstraint(key).allowedTiles();
-        Set<NeighborPair<Note>> notePairs = ConstraintUtils
-                .applyHardConstraint(WFCUtils.allCombinations(noteOptions),
-                        new MelodyStepSizeHardConstraint(Set.of(1,2,3)));
-
-        for(Chord chord : chordProgression){
-            WFC<Note> noteWFC = new WFC<>(notePairs);
-            List<Note> melodySegment = noteWFC.generate(
-                    List.of(Optional.of(chord.getThird()), Optional.empty(), Optional.empty(), Optional.empty()));
-            System.out.println(chord + " - " + melodySegment);
-        }
-
-                 */
     }
 }
