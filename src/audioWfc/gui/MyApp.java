b1d2fae03adc819b4c35fb9e5aa9a138fd7053d4
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2023-03-21 14:42:46 +0100

    Nicer GUI!

--- src/audioWfc/gui/MyApp.java
+++ src/audioWfc/gui/MyApp.java
@@ -13,13 +13,16 @@ import audioWfc.wfc.constraints.ChordInKeyConstraint;
 import audioWfc.wfc.constraints.ChordStepSizeHardConstraint;
 import audioWfc.wfc.constraints.DescendingMelodySoftConstraint;
 import audioWfc.wfc.constraints.MelodyAbsoluteStepSizeHardConstraint;
+import audioWfc.wfc.constraints.MelodyShapeHardConstraint;
 import audioWfc.wfc.constraints.MelodyStartsOnNoteHardConstraint;
 import audioWfc.wfc.constraints.NoteInKeyHardConstraint;
 import audioWfc.wfc.constraints.NoteInOctavesConstraint;
 import audioWfc.wfc.constraints.PerfectCadenceSoftConstraint;
 import audioWfc.wfc.constraints.PlagalCadenceSoftConstraint;
 import audioWfc.wfc.constraints.concepts.Constraint;
+import audioWfc.wfc.constraints.concepts.MelodyShape;
 import audioWfc.wfc.grabbers.BasicKeyGrabber;
+import audioWfc.wfc.grabbers.BasicMelodyShapeGrabber;
 import audioWfc.wfc.grabbers.FifthOfChordGrabber;
 import audioWfc.wfc.grabbers.Grabber;
 import audioWfc.wfc.grabbers.IntegerSetConstantGrabber;
@@ -31,6 +34,7 @@ import audioWfc.wfc.hierarchy.NoteLevelNode;
 
 import java.awt.FlowLayout;
 import java.awt.event.*;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
@@ -41,16 +45,34 @@ import javax.swing.*;
 import static audioWfc.musicTheory.Note.C;
 
 public class MyApp extends JFrame {
+    public static final String MELODY_STEP_SIZES = "Melody step sizes";
+    public static final String MELODY_IN_KEY = "Melody in key";
+    public static final String ASCENDING_MELODY = "Ascending melody";
+    public static final String DESCENDING_MELODY = "Descending melody";
+    public static final String START_MELODY_ON_NOTE = "Start melody on note";
+    public static final String MELODY_IN_OCTAVES = "Melody in octaves";
+    public static final String MELODY_SHAPE = "Melody shape";
+    public static final String CHORDS_IN_KEY = "Chords in key";
+    public static final String DISTANCES_BETWEEN_ADJACENT_CHORDS = "Distances between adjacent chords";
+    public static final String PERFECT_CADENCES = "Perfect cadences";
+    public static final String PLAGAL_CADENCES = "Plagal cadences";
+    public static final String CHORD_ROOT = "Root of current chord";
+    public static final String CHORD_THIRD = "Third of current chord";
+    public static final String CHORD_FIFTH = "Fifth of current chord";
+
     private JComboBox<String> noteConstraintComboBox;
     private JComboBox<String> chordConstraintComboBox;
     private JComboBox<String> noteGrabberComboBox;
     private JButton addNoteConstraintButton;
     private JButton addChordConstraintButton;
+    private JButton configureNoteConstraintButton;
+    private JButton configureChordConstraintButton;
     private JButton generateButton;
     private JButton resetButton;
     private JLabel constraintsLabel;
     private TextFieldWithTitle weightTextField;
     private TextFieldWithTitle integerSetTextField;
+    private TextFieldWithTitle melodyShapeTextField;
 
     private ConstraintSet<OctavedNote> noteConstraints;
     private OptionsPerCell<OctavedNote> noteOptionsPerCell;
@@ -84,30 +106,32 @@ public class MyApp extends JFrame {
         setLayout(new FlowLayout());
 
         noteConstraintComboBox = new JComboBox<>(new String[]{
-                "Melody in key",
-                "Melody step sizes",
-                "Ascending melody",
-                "Descending melody",
-                "Start melody on note",
-                "Melody in octaves"
+                MELODY_IN_KEY,
+                MELODY_STEP_SIZES,
+                ASCENDING_MELODY,
+                DESCENDING_MELODY,
+                START_MELODY_ON_NOTE,
+                MELODY_IN_OCTAVES,
+                MELODY_SHAPE
         });
         add(noteConstraintComboBox);
 
-        addNoteConstraintButton = new JButton("Add note constraint");
-        addNoteConstraintButton.addActionListener(this::addNoteConstraint);
-        add(addNoteConstraintButton);
+        configureNoteConstraintButton = new JButton("Configure note constraint");
+        configureNoteConstraintButton.addActionListener(this::configureNoteConstraint);
+        add(configureNoteConstraintButton);
+
 
         chordConstraintComboBox = new JComboBox<>(new String[]{
-                "Chords in key",
-                "Distances between adjacent chords",
-                "Perfect cadences",
-                "Plagal cadences"
+                CHORDS_IN_KEY,
+                DISTANCES_BETWEEN_ADJACENT_CHORDS,
+                PERFECT_CADENCES,
+                PLAGAL_CADENCES
         });
         add(chordConstraintComboBox);
 
-        addChordConstraintButton = new JButton("Add chord constraint");
-        addChordConstraintButton.addActionListener(this::addChordConstraint);
-        add(addChordConstraintButton);
+        configureChordConstraintButton = new JButton("Configure chord constraint");
+        configureChordConstraintButton.addActionListener(this::configureChordConstraint);
+        add(configureChordConstraintButton);
 
         weightTextField = new TextFieldWithTitle("Soft constraint weights");
         add(weightTextField);
@@ -115,12 +139,23 @@ public class MyApp extends JFrame {
         integerSetTextField = new TextFieldWithTitle("Integer set");
         add(integerSetTextField);
 
+        melodyShapeTextField = new TextFieldWithTitle(MELODY_SHAPE);
+        add(melodyShapeTextField);
+
         noteGrabberComboBox = new JComboBox<>(new String[]{
-                "Root of current chord",
-                "Third of current chord",
-                "Fifth of current chord"
+                CHORD_ROOT,
+                CHORD_THIRD,
+                CHORD_FIFTH
         });
         add(noteGrabberComboBox);
+
+        addNoteConstraintButton = new JButton("Add note constraint");
+        addNoteConstraintButton.addActionListener(this::addNoteConstraint);
+        add(addNoteConstraintButton);
+
+        addChordConstraintButton = new JButton("Add chord constraint");
+        addChordConstraintButton.addActionListener(this::addChordConstraint);
+        add(addChordConstraintButton);
 
         generateButton = new JButton("Generate");
         generateButton.addActionListener(this::generate);
@@ -133,6 +168,7 @@ public class MyApp extends JFrame {
         constraintsLabel = new JLabel("\n");
         add(constraintsLabel);
 
+        enterMainMode();
         setVisible(true);
     }
 
@@ -171,13 +207,18 @@ public class MyApp extends JFrame {
         return new ChordStepSizeHardConstraint(new IntegerSetConstantGrabber(integerSet));
     }
 
+    public MelodyShapeHardConstraint createMelodyShapeHardConstraint(){
+        MelodyShape shape = MelodyShape.parse(melodyShapeTextField.getText());
+        return new MelodyShapeHardConstraint(new BasicMelodyShapeGrabber(shape));
+    }
+
     private Grabber<Note> createNoteGrabber(){
         String selectedOption = (String) noteGrabberComboBox.getSelectedItem();
         Grabber<Note> out;
         switch (selectedOption) {
-            case "Root of current chord" -> {out = new RootOfChordGrabber();}
-            case "Third of current chord" -> {out = new ThirdOfChordGrabber();}
-            case "Fifth of current chord" -> {out = new FifthOfChordGrabber();}
+            case CHORD_ROOT -> {out = new RootOfChordGrabber();}
+            case CHORD_THIRD -> {out = new ThirdOfChordGrabber();}
+            case CHORD_FIFTH -> {out = new FifthOfChordGrabber();}
             default -> throw new RuntimeException("Unknown option");
         }
         return out;
@@ -199,42 +240,132 @@ public class MyApp extends JFrame {
         System.out.println(s);
     }
 
-    public void addNoteConstraint(ActionEvent e) {
+    private void enterConfigMode(){
+        Set<JComponent> componentsToHide = Set.of(
+                noteConstraintComboBox,
+                configureNoteConstraintButton,
+                chordConstraintComboBox,
+                configureChordConstraintButton,
+                resetButton,
+                generateButton
+        );
+        for(JComponent component : componentsToHide){
+            component.setVisible(false);
+        }
+    }
+
+    private void enterMainMode(){
+        Set<JComponent> componentsToHide = Set.of(
+                addNoteConstraintButton,
+                addChordConstraintButton,
+                integerSetTextField,
+                melodyShapeTextField,
+                weightTextField,
+                noteGrabberComboBox
+        );
+        for(JComponent component : componentsToHide){
+            component.setVisible(false);
+        }
+        Set<JComponent> componentsToShow = Set.of(
+                noteConstraintComboBox,
+                chordConstraintComboBox,
+                configureNoteConstraintButton,
+                configureChordConstraintButton,
+                generateButton,
+                resetButton,
+                constraintsLabel
+        );
+        for(JComponent component : componentsToShow){
+            component.setVisible(true);
+        }
+    }
+
+    public void configureNoteConstraint(ActionEvent e) {
         String selectedOption = (String) noteConstraintComboBox.getSelectedItem();
         if(constraintsLabel.getText().contains(selectedOption)){
             alert("Constraint already exists");
         } else {
-            Constraint<OctavedNote> newConstraint;
+            enterConfigMode();
+            Set<JComponent> componentsToShow = new HashSet<>();
             switch (selectedOption) {
-                case "Melody in key" -> {newConstraint = new NoteInKeyHardConstraint(new BasicKeyGrabber());}
-                case "Melody step sizes" -> {newConstraint = createMelodyStepSizeHardConstraint();}
-                case "Ascending melody" -> {newConstraint = createAscendingMelodySoftConstraint();}
-                case "Descending melody" -> {newConstraint = createDescendingMelodySoftConstraint();}
-                case "Start melody on note" -> {newConstraint = new MelodyStartsOnNoteHardConstraint(createNoteGrabber());}
-                case "Melody in octaves" -> {newConstraint = createNoteInOctavesConstraint();}
-                default -> throw new RuntimeException("Unknown option");
+                case MELODY_STEP_SIZES, MELODY_IN_OCTAVES -> componentsToShow.add(integerSetTextField);
+                case ASCENDING_MELODY, DESCENDING_MELODY -> componentsToShow.add(weightTextField);
+                case START_MELODY_ON_NOTE -> componentsToShow.add(noteGrabberComboBox);
+                case MELODY_SHAPE -> componentsToShow.add(melodyShapeTextField);
             }
-            noteConstraints.addConstraint(newConstraint);
-            constraintsLabel.setText(multiline(constraintsLabel.getText()+ selectedOption + "\n" ));
+            if(componentsToShow.isEmpty()){
+                addNoteConstraint();
+                return;
+            }
+            componentsToShow.add(addNoteConstraintButton);
+            for(JComponent component : componentsToShow){
+                component.setVisible(true);
+            }
         }
     }
 
-    public void addChordConstraint(ActionEvent e) {
+    public void addNoteConstraint(ActionEvent e) {
+        addNoteConstraint();
+    }
+
+    private void addNoteConstraint() {
+        String selectedOption = (String) noteConstraintComboBox.getSelectedItem();
+
+        Constraint<OctavedNote> newConstraint;
+        switch (selectedOption) {
+            case MELODY_IN_KEY -> {newConstraint = new NoteInKeyHardConstraint(new BasicKeyGrabber());}
+            case MELODY_STEP_SIZES -> {newConstraint = createMelodyStepSizeHardConstraint();}
+            case ASCENDING_MELODY -> {newConstraint = createAscendingMelodySoftConstraint();}
+            case DESCENDING_MELODY -> {newConstraint = createDescendingMelodySoftConstraint();}
+            case START_MELODY_ON_NOTE -> {newConstraint = new MelodyStartsOnNoteHardConstraint(createNoteGrabber());}
+            case MELODY_IN_OCTAVES -> {newConstraint = createNoteInOctavesConstraint();}
+            case MELODY_SHAPE -> {newConstraint = createMelodyShapeHardConstraint();}
+            default -> throw new RuntimeException("Unknown option");
+        }
+        noteConstraints.addConstraint(newConstraint);
+        constraintsLabel.setText(multiline(constraintsLabel.getText()+ selectedOption + "\n" ));
+        enterMainMode();
+    }
+
+    public void configureChordConstraint(ActionEvent e) {
         String selectedOption = (String) chordConstraintComboBox.getSelectedItem();
         if(constraintsLabel.getText().contains(selectedOption)){
             alert("Constraint already exists");
         } else {
-            Constraint<Chord> newConstraint;
+            enterConfigMode();
+            Set<JComponent> componentsToShow = new HashSet<>();
             switch (selectedOption) {
-                case "Chords in key" -> {newConstraint = new ChordInKeyConstraint(new BasicKeyGrabber());}
-                case "Distances between adjacent chords" -> {newConstraint = createChordStepSizeConstraint();}
-                case "Perfect cadences" -> {newConstraint = createPerfectCadenceSoftConstraint();}
-                case "Plagal cadences" -> {newConstraint = createPlagalCadenceSoftConstraint();}
-                default -> throw new RuntimeException("Unknown option");
+                case DISTANCES_BETWEEN_ADJACENT_CHORDS -> {componentsToShow.add(integerSetTextField);}
+                case PERFECT_CADENCES, PLAGAL_CADENCES -> {componentsToShow.add(weightTextField);}
             }
-            chordConstraints.addConstraint(newConstraint);
-            constraintsLabel.setText(multiline(constraintsLabel.getText()+ selectedOption + "\n" ));
+            if(componentsToShow.isEmpty()){
+                addChordConstraint();
+                return;
+            }
+            componentsToShow.add(addChordConstraintButton);
+            for(JComponent component : componentsToShow){
+                component.setVisible(true);
+            }
         }
+    }
+
+    public void addChordConstraint(ActionEvent e) {
+        addChordConstraint();
+    }
+
+    private void addChordConstraint() {
+        String selectedOption = (String) chordConstraintComboBox.getSelectedItem();
+        Constraint<Chord> newConstraint;
+        switch (selectedOption) {
+            case CHORDS_IN_KEY -> {newConstraint = new ChordInKeyConstraint(new BasicKeyGrabber());}
+            case DISTANCES_BETWEEN_ADJACENT_CHORDS -> {newConstraint = createChordStepSizeConstraint();}
+            case PERFECT_CADENCES -> {newConstraint = createPerfectCadenceSoftConstraint();}
+            case PLAGAL_CADENCES -> {newConstraint = createPlagalCadenceSoftConstraint();}
+            default -> throw new RuntimeException("Unknown option");
+        }
+        chordConstraints.addConstraint(newConstraint);
+        constraintsLabel.setText(multiline(constraintsLabel.getText()+ selectedOption + "\n" ));
+        enterMainMode();
     }
 
     public void generate(ActionEvent e) {
---
+++ src/audioWfc/wfc/grabbers/BasicMelodyShapeGrabber.java
@@ -0,0 +1,17 @@
+package audioWfc.wfc.grabbers;
+
+import audioWfc.wfc.HigherValues;
+import audioWfc.wfc.constraints.concepts.MelodyShape;
+
+public class BasicMelodyShapeGrabber implements Grabber<MelodyShape>{
+    private MelodyShape melodyShape;
+
+    public BasicMelodyShapeGrabber(MelodyShape melodyShape){
+        this.melodyShape = melodyShape;
+    }
+
+    @Override
+    public MelodyShape grab(HigherValues higherValues) {
+        return melodyShape;
+    }
+}
