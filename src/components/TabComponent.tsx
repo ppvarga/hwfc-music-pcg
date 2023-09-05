429c60fab1a666716d62899862b77f2223ccd7e6
Pál Patrik Varga <VargaPalPatrik@student.tudelft.nl>
2023-09-05 11:38:52 +0200

    basic tabs

---
+++ src/components/TabComponent.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import Tab from "./Tab"
+
+interface TabItem {
+  title: string;
+  content: JSX.Element;
+}
+
+interface TabComponentProps {
+  tabs: TabItem[];
+}
+
+const TabComponent: React.FC<TabComponentProps> = ({ tabs }) => {
+	const [activeTabIndex, setActiveTabIndex] = useState(0)
+
+	return (
+		<div style={{display: "flex", flexDirection: "column", height: "100vh", width: "80vw"}}>
+			<div style={{ display: "flex"}}>
+				{tabs.map((tab, index) => (
+					<>
+						<div style={{width: "10px", borderBottom: "1px white solid"}}/>
+						<Tab
+							key={index}
+							title={tab.title}
+							isActive={index === activeTabIndex}
+							onClick={() => setActiveTabIndex(index)}
+						/>
+					</>
+				))}
+				<div style={{flexGrow: 10, borderBottom: "1px white solid"}}/>
+			</div>
+            
+			<div style={{overflowY: "auto", flexGrow: 1}}>
+				{tabs[activeTabIndex].content}
+			</div>
+		</div>
+	)
+}
+
+export default TabComponent
